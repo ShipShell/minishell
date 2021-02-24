a65be17964226d449b827914e817a67b85d21628
Likilee <gnsdlrl@daum.net>
2021-02-24 15:18:44 +0900

    Update kilee_prac

--- TEAM_1/kilee_prac/src/main.c
+++ TEAM_1/kilee_prac/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kilee <kilee@student.42.fr>                +#+  +:+       +#+        */
+/*   By: kihoonlee <kihoonlee@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 12:43:44 by kilee             #+#    #+#             */
-/*   Updated: 2021/02/23 16:34:53 by kilee            ###   ########.fr       */
+/*   Updated: 2021/02/24 14:50:11 by kihoonlee        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,31 @@ void	cntlc(int signo)
 	signal(SIGINT, SIG_DFL);
 }
 
+
+/*
+** 프롬프트 띄우는 함수
+** 이름 : prompt_show()
+** 1. shipshell$
+*/
+// int		main(int argc, char *argv[], char *envp[])
+// {
+// 	int		i;
+
+// 	i = -1;
+// 	signal(SIGINT, cntlc);
+// 	while(1)
+// 	;
+// 	return (0);
+// }
+
 int		main(int argc, char *argv[], char *envp[])
 {
 	int		i;
 
 	i = -1;
-	signal(SIGINT, cntlc);
-	while(1)
-	;
+	while(envp[++i])
+	{
+		printf("%s\n", envp[i]);
+	}
 	return (0);
 }
