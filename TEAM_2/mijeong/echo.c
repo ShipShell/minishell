3015d22523d60c0bdf39ce59e0578949120f79ca
dev_mj <dev_mj@dev-mjui-MacBookPro.local>
2021-03-01 22:50:44 +0900

    add mijeong exec file

--- TEAM_2/mijeong/echo.c
+++ TEAM_2/mijeong/echo.c
@@ -1,16 +1,35 @@
 #include "exec.h"
 
-int	put_arg_to_std()
+int	put_arg_to_std(t_data *data, int option)
 {
+	t_list	*arg_list;
 
+	arg_list = data->arg;
+	if ((char *)arg_list->content)
+	{
+		ft_putstr_fd((char *)arg_list->content, 1);
+		arg_list = arg_list->next;
+	}
+	arg_list = arg_list->next;
+	while ((char *)data->arg->content)
+	{
+		write(1, " ", 1);
+		ft_putstr_fd((char *)arg_list->content, 1);
+		arg_list = arg_list->next;
+	}
+	if (option)
+		ft_putstr_fd("\n", 1);
+	return (0);
 }
 
 int	ft_echo(t_data *data)
 {
-	while (((char *)data->arg->content))
-	{
-		put_arg_to_std();
-		
-	}
-	data->arg
+	int	option;
+
+	option = 0;
+	if (!ft_strcmp((char *)data->arg->content, "-n"))
+		option = 1;
+	put_arg_to_std(data, option);
+	
+	return (0);
 }
---
+++ TEAM_2/mijeong/env.c
@@ -0,0 +1,6 @@
+# include "exec.h"
+
+int env(t_data *data)
+{
+	
+}
