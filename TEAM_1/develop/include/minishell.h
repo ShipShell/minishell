a7ea3323a1c08ba29853204aa2b1feda0c811716
Heewon Son <hson@c4r2s6.42seoul.kr>
2021-03-08 10:34:24 +0900

    tmp

--- TEAM_1/develop/include/minishell.h
+++ TEAM_1/develop/include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sonheewon <sonheewon@student.42.fr>        +#+  +:+       +#+        */
+/*   By: hson <hson@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 16:11:23 by kilee             #+#    #+#             */
-/*   Updated: 2021/03/06 17:28:38 by sonheewon        ###   ########.fr       */
+/*   Updated: 2021/03/08 10:31:52 by hson             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@
 extern t_env	*g_env;
 extern t_cmd	*g_cmd;
 extern int		g_exit_code;
-// pipe를 위해 임시로!!
+// pipe를 위해 로!!
 extern int		g_pipe;
 
 int		prompt_show(void);
