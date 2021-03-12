8ddfc4829fdd55f6e360f21430e43d6ed7f96265
dev_mj <dev_mj@dev-mjui-MacBookPro.local>
2021-03-12 13:14:17 +0900

    need norm check

--- TEAM_2/submit/main.c
+++ TEAM_2/submit/main.c
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeonkim <hyeonkim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dev_mj <dev_mj@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/26 09:00:23 by hyeonkim          #+#    #+#             */
-/*   Updated: 2021/03/05 16:27:52 by hyeonkim         ###   ########.fr       */
+/*   Updated: 2021/03/12 12:39:01 by dev_mj           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "minishell.h"
 
 void	show_prompt()
@@ -22,12 +23,24 @@ void	exit_by_cntl_d(void)
 	exit(0);
 }
 
+char	*join_and_abandon(char *str1, char *str2)
+{
+	char	*garbage;
+
+	if (str1 == 0 || str2 == 0)
+		return (0);
+	garbage = str1;
+	str1 = ft_strjoin(str1, str2);
+	free(garbage);
+	free(str2);
+	return (str1);
+}
+
 int		wait_cmd(void)
 {
 	char	*stdin_buf;
 	t_list	*tokenized_single_cmd_list;
 	char	*tmp;
-	char	*garbage;
 	int		len;
 
 	tmp = 0;
@@ -35,27 +48,20 @@ int		wait_cmd(void)
 	while (get_next_line(0, &stdin_buf) == 0)
 	{
 		len = ft_strlen(stdin_buf);
-		// printf("tmp: %s\n", tmp);
-		if (tmp == 0 && len == 0)
-			exit_by_cntl_d();
-		else if (len != 0)
+		if (len)
 		{
-			garbage = tmp;
-			if (tmp == NULL)
+			if (tmp == 0)
 				tmp = ft_strdup(stdin_buf);
 			else
-			{
-				tmp = ft_strjoin(tmp, stdin_buf);
-				free(garbage);
-			}
+				tmp = join_and_abandon(tmp, stdin_buf);
 		}
+		if (len == 0 && tmp == 0)
+			exit_by_cntl_d();
 		ft_putstr_fd("  \b\b  \b\b", 1);
 	}
-	if (tmp)
-		tmp = ft_strjoin(tmp, stdin_buf);
-
+	if (tmp != 0)
+		stdin_buf = join_and_abandon(tmp, stdin_buf);
 	tokenized_single_cmd_list = parse_cmd_line(stdin_buf);
-	// substitute_token(tokenized_single_cmd_list);
 	// tokenized_single_cmd_list 가 t_cmd(**command 랑 flag, exit_code가 담긴)의 연결리스트임!
 	cycle_list(tokenized_single_cmd_list);
 	free(stdin_buf);
@@ -81,15 +87,6 @@ void	handle_sigint(int signo)
 	return ;
 }
 
-// void	handle_sigterm(int signo)
-// {
-// 	(void)signo;
-// 	if (g_child)
-// 		ft_putstr_fd("exit\n", 1);
-// 	exit(0);
-// 	return ;
-// }
-
 void	handle_sigquit(int signo)
 {
 	(void)signo;
@@ -109,7 +106,6 @@ int		main(int argc, char **argv, char **envp)
 	g_child = 0;
 	signal(SIGINT, handle_sigint);
 	signal(SIGQUIT, handle_sigquit);
-	// signal(SIGTERM, handle_sigterm);
 	set_env_list(envp);
 	prompt_loop();
 	return (0);
