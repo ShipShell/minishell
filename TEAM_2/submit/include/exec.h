401bac9d7d208d89b241bc34b10cf4b101ea0718
dev_mj <dev_mj@dev-mjui-MacBookPro.local>
2021-03-12 14:50:03 +0900

    t

--- TEAM_2/submit/include/exec.h
+++ TEAM_2/submit/include/exec.h
@@ -13,44 +13,54 @@
 /*
 ** error
 */
-int	print_command_not_found_err(t_cmd *cmd, int index);
-int	print_no_such_file_err(char *filename);
-int	print_strerror(char *);
-int	print_not_a_valid_identifier(char *str);
-int	cycle_list(t_list *cmd_list);
+int		print_command_not_found_err(t_cmd *cmd, int index);
+int		print_no_such_file_err(char *filename);
+int		print_strerror(char *);
+int		print_not_a_valid_identifier(char *str);
+int		cycle_list(t_list *cmd_list);
 
+/*
+** exec
+*/
+int		is_built_in(t_cmd *cmd);
+int		exec_command(t_cmd *cmd);
+int		exec_pipe_command(t_cmd *cmd);
+int		count_pipe(t_list *cmd_list);
 /*
 ** built_in_command
 */
-int	ft_env(t_cmd *cmd);
-int	ft_echo(t_cmd *cmd);
-int	ft_pwd();
-int	ft_unset(t_cmd *cmd);
-int	ft_exit(t_cmd *cmd);
-int	exec_command(t_cmd *cmd);
+int		ft_env(t_cmd *cmd);
+int		ft_echo(t_cmd *cmd);
+int		ft_pwd();
+int		ft_unset(t_cmd *cmd);
+int		ft_exit(t_cmd *cmd);
+int		exec_command(t_cmd *cmd);
 
 /*
 ** ridir
 */
 int	ft_redir(t_cmd *cmd);
 int	get_fd_back(t_cmd *cmd);
+int	open_redir_in(t_cmd *cmd, t_redir *redir);
 
 /*
 ** export
 */
-int	ft_export(t_cmd *cmd);
-int	cycle_arg(t_cmd *cmd);
+int		ft_export(t_cmd *cmd);
+int		cycle_arg(t_cmd *cmd);
+t_env	*is_same_env(char *str);
 
 /*
 ** cd
 */
-int	ft_cd(t_cmd *cmd);
-int	call_no_home_err();
+int		ft_cd(t_cmd *cmd);
+int		call_no_home_err(void);
+int		put_env_pwd(char *current_dir, t_list *pwd_lst);
 
 /*
 ** not_built_in
 */
-int	exec_not_built_in(t_cmd *cmd);
-int	ft_not_built_in(t_cmd *cmd);
+int		exec_not_built_in(t_cmd *cmd);
+int		ft_not_built_in(t_cmd *cmd);
 
 #endif
