7bcba3e6399fd654b7f80e8ff0b2479104ccfec5
hyongti <hyongtiii@gmail.com>
2021-03-15 10:20:28 +0900

    modify redirection.c

--- TEAM_2/submit/include/free.h
+++ TEAM_2/submit/include/free.h
@@ -1,29 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   free.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyeonkim <hyeonkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/12 14:06:24 by hyeonkim          #+#    #+#             */
-/*   Updated: 2021/03/15 08:24:13 by hyeonkim         ###   ########.fr       */
+/*   Created: 2021/03/15 08:23:42 by hyeonkim          #+#    #+#             */
+/*   Updated: 2021/03/15 08:34:33 by hyeonkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef FREE_H
+# define FREE_H
 
-# include "structures.h"
-# include "libft.h"
-# include "exec.h"
-# include "parse.h"
-# include "free.h"
-
-void	prompt_show(void);
-void	prompt_loop(void);
-void	handle_sigint(int signo);
-t_env	*set_env(char *envp);
-void	set_env_list(char **envp);
-int		command_len(char *stdin_buf);
+void	free_used_double_pointer(char **str);
+void	free_used_str_list(t_list *tmp_token);
 
 #endif
